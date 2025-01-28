e1f0503b4d92a9e7722699bdf9c99b2ad3b444d9
Neeraj Yadav <neeraj95578@gmail.com>
2025-01-28 10:16:36 +0530

    routing changes completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import { PublicLayout } from "@/layouts/public-layout";
+import AuthenticationLayout from "./layouts/auth-layout";
+import ProtectedRoutes from "./layouts/protected-routes";
+
+import HomePage from "@/routes/home";
+import { SignInPage } from "./routes/sign-in";
+import { SignUpPage } from "./routes/sign-up";
+import { MainLayout } from "./layouts/main-layout";
+
 const App = () => {
-  return <div className="text-blue-500">hello</div>;
+  return (
+    <Router>
+      <Routes>
+        {/* pullic routes */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        {/* authenctication layout */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+
+        {/* protected routes */}
+        <Route
+          element={
+            <ProtectedRoutes>
+              <MainLayout />
+            </ProtectedRoutes>
+          }
+        >
+          {/* all protected routes */}
+          
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
